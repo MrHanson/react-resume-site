6e5619c3b7fa019bda07e4d485be224aac6f755e
mrhanson <chihanghanson@gmail.com>
2021-04-21 21:36:54 +0800

    perf: remove useless variable

--- src/components/HeaderBar/index.tsx
+++ src/components/HeaderBar/index.tsx
@@ -1,27 +1,12 @@
-import React, { useState, useEffect, useRef } from "react";
-import {
-  Menu,
-  Dropdown,
-  message,
-  Modal,
-  Form,
-  Switch,
-  Input,
-  FormInstance,
-} from "antd";
+import React, { useEffect } from "react";
+import { Menu, Dropdown } from "antd";
 
 import "./index.less";
 import { getTheme } from "@utils/changeThemes";
-import {
-  markdownParserResume,
-  downloadDirect,
-  downloadFetch,
-} from "@utils/helper";
-import { getPdf } from "@src/service/htmlToPdf";
+import { downloadDirect } from "@utils/helper";
 import { useStores } from "@src/store";
 import { mdEditorRef } from "@src/utils/global";
 import svgMap from "@src/utils/svgMap";
-import { LOCAL_STORE } from "@src/utils/const";
 
 const themes = [
   {
@@ -38,10 +23,6 @@ const default_theme = themes[0].id;
 
 const HeaderBar = () => {
   const { templateStore } = useStores();
-  const [template, setTemplate] = useState(default_theme);
-  const [isExportVisible, setIsExportVisible] = useState(false);
-
-  const formRef = useRef<FormInstance>(null);
 
   const uploadMdFile = (e: any) => {
     let resultFile = e.target.files[0];
@@ -81,51 +62,6 @@ const HeaderBar = () => {
       </Menu.Item>
     </Menu>
   );
-
-  const handleExport = () => {
-    formRef.current?.submit();
-    setIsExportVisible(false);
-  };
-
-  const exportPdf = async ({
-    name,
-    isOnePage,
-    isMark,
-  }: {
-    name: string;
-    isOnePage: boolean;
-    isMark: boolean;
-  }) => {
-    const content = localStorage.getItem(LOCAL_STORE.MD_RESUME);
-    if (content) {
-      const htmlContent = markdownParserResume
-        .render(content)
-        .replace(/(\n|\r)/g, "");
-      const theme = template;
-      let hide = message.loading("正在为你生成简历...", 0);
-      const themeColor = getComputedStyle(document.body).getPropertyValue(
-        "--bg"
-      );
-      try {
-        let data = await getPdf({
-          htmlContent,
-          theme,
-          themeColor,
-          isMark,
-          isOnePage,
-        });
-        await downloadFetch(
-          data.url,
-          name ? `${name}.pdf` : "web前端开发 梁智恒Hanson.pdf"
-        );
-        hide();
-        message.success("恭喜你，导出成功!");
-      } catch (e) {
-        hide();
-        message.error("生成简历出错，请稍再试!");
-      }
-    }
-  };
 
   useEffect(() => {
     getTheme(default_theme);
