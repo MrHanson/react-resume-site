db16269b22b2a454e70762cdd4c1e272268e1489
mrhanson <chihanghanson@gmail.com>
2021-04-22 23:26:34 +0800

    fix: changeThemes baseUrl

--- src/utils/changeThemes.ts
+++ src/utils/changeThemes.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function getTheme(type: string) {
   console.log('getTheme', type)
-  const base = 'react-resume-site';
+  const base = process.env.NODE_ENV === 'development' ? 'react-resume-site' : '';
   const result = await axios.get(`${base}/themes/${type}.css`);
   const themesStyles = document.getElementById('rs-themes-data');
   if (themesStyles) {
